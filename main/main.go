f7473ea5fab41546f43d698792d6a33628b7c656
sanfen.mz <mazhou@likingfit.com>
2017-08-25 17:37:53 +0800

    添加注释

--- main/main.go
+++ main/main.go
@@ -12,6 +12,8 @@ func main() {
 	i := 0
 	j := 0
 	k := 0
+
+	//添加任务1  定时任务
 	_, job1 := c.AddJob(spec, cron.Job{
 		Count: -1,
 		Run: func() {
@@ -19,6 +21,8 @@ func main() {
 			i ++
 		},
 	})
+
+	//添加任务2 固定5次
 	c.AddJob(spec, cron.Job{
 		Count: 5,
 		Run: func() {
@@ -30,6 +34,7 @@ func main() {
 
 	time.Sleep(time.Second * 10)
 
+	//添加任务3
 	c.AddJob(spec, cron.Job{
 		Count: -1,
 		Run: func() {
@@ -38,6 +43,9 @@ func main() {
 		},
 	})
 
+	//移除任务1
 	c.RemoveSchedule(job1)
+
+
 	select {}
 }
