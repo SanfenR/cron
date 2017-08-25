bec93d43aae3721aeb9b8719bec34784e6b12d42
sanfen.mz <mazhou@likingfit.com>
2017-08-25 17:31:00 +0800

    添加测试

--- main/main.go
+++ main/main.go
@@ -3,25 +3,41 @@ package main
 import (
 	"cron"
 	"log"
+	"time"
 )
 
 func main() {
-
 	c := cron.New()
-
 	spec := "*/1 * * * * *"
 	i := 0
-
-	c.AddJob(spec, cron.Job{
-		Count : 5,
-		Run: func(){
-			log.Println("start ", i)
+	j := 0
+	k := 0
+	_, job1 := c.AddJob(spec, cron.Job{
+		Count: -1,
+		Run: func() {
+			log.Println("work 1 ", i)
 			i ++
 		},
 	})
-
+	c.AddJob(spec, cron.Job{
+		Count: 5,
+		Run: func() {
+			log.Println("work 2 ", j)
+			j ++
+		},
+	})
 	c.Start()
 
+	time.Sleep(time.Second * 10)
 
+	c.AddJob(spec, cron.Job{
+		Count: -1,
+		Run: func() {
+			log.Println("work 3 ", k)
+			k ++
+		},
+	})
+
+	c.RemoveSchedule(job1)
 	select {}
 }
